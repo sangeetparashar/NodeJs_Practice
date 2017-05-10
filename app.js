831eca6af91ff2619d3fb46c5f7d1bf2b8f9a073
Sangeet Parashar <sparasha@uwo.ca>
2017-05-09 21:45:30 -0400

    learning the basic application of lodash - a 3rd party module

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ console.log("Hello world");
 const fs = require('fs');
 const os = require('os');
 const user = os.userInfo();
+const _ = require('lodash');
 
 //loading our own module
 const notes = require('./notes.js');
-const result = notes.add(9,-2);
+//const result = notes.add(9,-2);
+console.log(_.isString(12312));
 
-console.log(`Result: ${result}`);
+console.log(_.isString("This is a strnig right?"));
 
-fs.appendFile('Notes.txt', `\nHello ${user.username}. You are ${notes.age}`, function (err) {
-    if (err) {
-        console.log("Couldn't write to the file!");
-    }
-});
+var filteredArray = ['Sangeet', 1, 2, 3, 1, 2, 424, 'Sangeet'];
+
+console.log(_.uniq(filteredArray));
+//console.log(`Result: ${result}`);
+
+//fs.appendFile('Notes.txt', `\nHello ${user.username}. You are ${notes.age}`, function (err) {
+//    if (err) {
+//        console.log("Couldn't write to the file!");
+//    }
+//});
