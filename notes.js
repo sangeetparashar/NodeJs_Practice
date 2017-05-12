1395551682ef72fcf2f6c22bc471ddb464eb0fe9
Sangeet Parashar <sparasha@uwo.ca>
2017-05-11 21:10:16 -0400

    proper add function has been added

--- notes.js
+++ notes.js
@@ -1,8 +1,30 @@
 console.log('Staring notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
-    console.log("adding note", title, body);
-}
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }
+    catch (e) {
+        Console.log("That file doesnt exist yet!");
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
+};
 var getAll = () => {
     console.log("Getting all notes");
 }
