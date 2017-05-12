5d2eadb78e608c68b457da99a62f45922e989299
Sangeet Parashar <sparasha@uwo.ca>
2017-05-11 20:00:30 -0400

    learning to use yargs and json objects

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 const os = require('os');
 const user = os.userInfo();
 const _ = require('lodash');
+var yargs = require('yargs');
 
 //loading our own module
 const notes = require('./notes.js');
-//const result = notes.add(9,-2);
-console.log(_.isString(12312));
+////const result = notes.add(9,-2);
+//console.log(_.isString(12312));
 
-console.log(_.isString("This is a strnig right?"));
+//console.log(_.isString("This is a strnig right?"));
 
-var filteredArray = ['Sangeet', 1, 2, 3, 1, 2, 424, 'Sangeet'];
+//var filteredArray = ['Sangeet', 1, 2, 3, 1, 2, 424, 'Sangeet'];
 
-console.log(_.uniq(filteredArray));
+//console.log(_.uniq(filteredArray));
 //console.log(`Result: ${result}`);
 
 //fs.appendFile('Notes.txt', `\nHello ${user.username}. You are ${notes.age}`, function (err) {
@@ -21,3 +22,29 @@ console.log(_.uniq(filteredArray));
 //        console.log("Couldn't write to the file!");
 //    }
 //});
+
+
+//we are about to create a nodeapp that gets user input inside the command line
+const argv = yargs.argv;
+
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process', process.argv); //argv = arguments vector
+console.log('Yargs', argv);
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+}
+else if (command === 'list') {
+    notes.getAll();
+}
+else if (command === 'read') {
+    notes.readNote(argv.title);
+}
+else if (command === 'remove') {
+    notes.removeNote (argv.title);
+}
+else {
+    console.log('Command not recognized');
+}
+
--- notes.js
+++ notes.js
@@ -1,6 +1,25 @@
-console.log('creating our own files to be required');
+console.log('Staring notes.js');
 
-module.exports.add = (a,b) => {
-    
-    return a+b;
+var addNote = (title, body) => {
+    console.log("adding note", title, body);
 }
+var getAll = () => {
+    console.log("Getting all notes");
+}
+
+var readNote = (title) => {
+    console.log("reading title: ", title);
+}
+
+var removeNote = (title) => {
+    console.log("Removing title: ", title);
+}
+
+
+module.exports = {
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+
+};
